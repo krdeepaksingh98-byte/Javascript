39537b2d6c0d32a9d793d1a05ebde494777d798a
Deepak singh <krdeepaksingh98@gmail.com>
2026-06-27 14:42:12 +0530

    2d project canvas try error line one

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    let canvas = document.getElementById("myCanvas")
+    let canvas = windowdocument.getElementById("myCanvas")
     let context = canvas.getContext("2d");
 
     var windowHeight = window.innerHeight;
